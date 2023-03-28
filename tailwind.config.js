09b2795b7b8fa4ef35c434a06275b59cdf5dd4d7
Ashutosh Anand <71189517+ashutoshanand058@users.noreply.github.com>
2023-03-28 11:36:37 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ module.exports = {
   plugins: [],
   screens: {
     xs: '480px',
-    sm: '640px', // => @media (min-width: 640px) { ... }
-    md: '768px', // => @media (min-width: 1024px) { ... }
-    lg: '1024px', // => @media (min-width: 1280px) { ... }
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
     xl: '1280px',
     '2xl': '1560px',
   },
